7c0140497c5b5303f6a4675d8f4d8936bc5caef7
Max <lxghoney@163.com>
2017-07-19 14:12:26 +0800

    modify ifTest shell

--- ifTest.sh
+++ ifTest.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+read input1
+echo -e "read to input:$input1\n"
 a=10
-
-if [ $a -lt 100 ]
-then
+function k(){
+ if [ $a -lt 100 ]
+ then
     echo '小于'
-else 
+ else 
     echo '大于'
-fi
+ fi
+}
+k
