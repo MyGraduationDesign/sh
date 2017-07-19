925916ea1ac19509c1e9d2cf26b4ffa1edd0739d
Max <lxghoney@163.com>
2017-07-19 14:22:34 +0800

    is empty add

--- ifTest.sh
+++ ifTest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo -e 'please entry other key:'
 read input1
 echo -e "read to input:$input1\n"
 a=10
@@ -11,4 +11,20 @@ function k(){
     echo '大于'
  fi
 }
+c='';
+d='xxxxxxxx';
+function isEmpty(){
+ if [ -z $c ]
+ then
+  echo -e '\033[1m$c is empty\n\033[0m'
+ fi
+
+ if [ -n $d ]
+ then
+  echo -e '\033[1m$d is not empty\n\033[0m'
+ fi
+}
+isEmpty
+
 k
+
