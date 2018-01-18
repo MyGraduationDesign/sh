d0eb3aad3b26a7bbe3db42b30decf189769e3bfa
Max <lxghoney@163.com>
2018-01-18 13:00:20 +0800

    add sql util

--- sql/oracle.sql
+++ sql/oracle.sql
@@ -23,3 +23,32 @@ PURGE TABLESPACE<<Table_NAME>> USER <<User_Name>>; --清空一个特定用户的
 PURGE RECYCLEBIN; --清空回收站
 DROP TABLE <<Table_Name>> PURGE;--当一个表被删除（drop）时就直接从回收站中清空
   
+  
+--锁表查询
+SELECT object_name, machine, s.sid, s.serial#
+  FROM gv$locked_object l, dba_objects o, gv$session s
+ WHERE l.object_id　 = o.object_id
+   AND l.session_id = s.sid;
+--验证
+SELECT 'ALTER system kill session ''' ||s.sid||','||s.serial#||''''
+  FROM gv$locked_object l, dba_objects o, gv$session s
+ WHERE l.object_id　 = o.object_id
+   AND l.session_id = s.sid;
+--杀掉
+ALTER system kill session '4055,34642';
+
+--存储过程终止
+--0.查询正在执行的存储过程
+select *
+  from v$db_object_cache
+ where locks > 0
+   and pins > 0
+   and type = 'PROCEDURE';
+--1. 在V$ACCESS视图中找到要停止进程的SID: 
+SELECT t.* FROM V$ACCESS t WHERE t.object='LXG_TOWERIDERR'; 
+SELECT SID FROM V$ACCESS WHERE  object='LXG_TOWERIDERR';
+--2. 在V$SESSION视图中查找到查出SID和SERIAL#
+SELECT SID,SERIAL# FROM V$SESSION WHERE SID='1314';  
+--3. 杀掉查找出来的进程
+--alter system kill session 'SID,SERIAL#'
+alter system kill session '1314,63759';  
